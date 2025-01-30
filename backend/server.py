41ed20eef0a54d372409d6ce4fdbb65232dd8f79
Kevin Johnsen <kmjohnsen@gmail.com>
2025-01-30 14:21:11 -0600

    server.py includes host 0.0.0.0

--- backend/server.py
+++ backend/server.py
@@ -52,4 +52,4 @@ def favicon():
     return send_from_directory('../frontend/resources', 'favicon.ico', mimetype='image/x-icon')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
