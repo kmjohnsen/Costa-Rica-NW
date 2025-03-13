a226c7943cc197737f7f0c26f241b89600e2bdd4
Kevin Johnsen <kmjohnsen@gmail.com>
2025-03-13 10:07:24 -0600

    fixed config.js file to point to costaricanorthwest.com

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:5000"
-    : "http://ec2-3-94-61-214.compute-1.amazonaws.com:5000"; // Replace with your EC2 public IP or domain
+    : "https://costaricanorthwest.com"; // Replace with your EC2 public IP or domain
 
 export default API_BASE_URL;
