3966ff8147398a9c84083e1903880c5b69a1abb7
Kevin Johnsen <kmjohnsen@gmail.com>
2025-03-15 11:13:07 -0600

    updated api call location in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://127.0.0.1:5000"
-    : "https://costaricanorthwest.com"; // Replace with your EC2 public IP or domain
+    : ""; //https://costaricanorthwest.com
 
 export default API_BASE_URL;
